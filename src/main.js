2c2d0cc2a578778a344763923bbefe2298d371e8
valtrig <12441016+valtrig@users.noreply.github.com>
2021-05-29 17:54:43 +0200

    Refactoring

--- src/main.js
+++ src/main.js
@@ -6,32 +6,26 @@ window.onload = function () {
 
   // Event handler for page buttons
   function handleOnclick(event) {
-    const SMS_BODY_KEY = 'smsBody';
-    var smsUri = event.currentTarget.value;
+    const button = event.currentTarget;
+    var smsUri = button.value;
 
     // Determine whether the clicked button contained a specific SMS URI.
     if (!isEmpty(smsUri)) {
-      // Retrieve the SMS body content from the browser's local storage and, if not found,
-      // prompt the user for it.
-      var smsBodyContent = localStorage.getItem(SMS_BODY_KEY);
-      if (isEmpty(smsBodyContent)) {
-        smsBodyContent = prompt(MESSAGE_INSERT_INFO);
+      // Detect if the device is using 'iOS' or 'macos' and use the appropiate character
+      // to append the SMS number part of the URL with the body content.
+      const isAppleDevice = ['iPad', 'iPhone', 'iPod'].includes(navigator.platform) || 
+        navigator.userAgent.includes('Mac');
+      smsUri += (isAppleDevice ? '&' : '?') + button.dataset.body;
+
+      // If additional details are required for the SMS body, use the specified attribute
+      // to retrieve and append them to the URL. 
+      if (button.dataset.details) {
+        smsUri = appendSmsBodyDetails(button.dataset.details, smsUri);
       }
 
-      // Determine whether a non-empty SMS body is available.
-      if (!isEmpty(smsBodyContent)) {
-        // (Re-)write the SMS body to local storage.
-        localStorage.setItem(SMS_BODY_KEY, smsBodyContent);
-
-        // Detect if the device is using 'iOS' or 'macos' and replace the default character
-        // that separates the SMS number from the body with the Apple specific one.
-        if (['iPad', 'iPhone', 'iPod'].includes(navigator.platform) || navigator.userAgent.includes('Mac')) {
-          smsUri = smsUri.replace('?', '&');
-        }
-
-        // Create the appropriate URL and open it so that it can be picked up by
-        // the device's SMS application.
-        window.open(smsUri + encodeURIComponent(smsBodyContent));
+      // Open the URI, if valid, so that it can be picked up by the device's SMS application.
+      if (smsUri) {
+        window.open(smsUri);
       }
     } else if (confirm(MESSAGE_DELETE_INFO)) {
       // Clear the local storage, if the clicked button contains no SMS code,
@@ -40,6 +34,30 @@ window.onload = function () {
 
       alert(MESSAGE_INFO_DELETED);
     }
+  }
+
+  function appendSmsBodyDetails(placeHolderName, smsUri) {
+    // Retrieve the SMS body content from the browser's local storage and, if not found,
+    // prompt the user for it.
+    var smsBodyContent = localStorage.getItem(placeHolderName);
+    if (isEmpty(smsBodyContent)) {
+      // Display the user prompt asking for the SMS body content.
+      smsBodyContent = prompt(MESSAGE_INSERT_INFO);
+    }
+
+    // Determine whether a non-empty SMS body is available.
+    if (!isEmpty(smsBodyContent)) {
+      // (Re-)write the SMS body to local storage.
+      localStorage.setItem(placeHolderName, smsBodyContent);
+
+      // Replace the placeholder in the URI with the SMS body content.
+      smsUri += encodeURIComponent(smsBodyContent);
+    } else {
+      // Reset the URI variable, since the required SMS body was not provided.
+      smsUri = null;
+    }
+
+    return smsUri;
   }
 
   function isEmpty(value) {
